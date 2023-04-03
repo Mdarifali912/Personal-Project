28ecc54b2b0e915c16d5b55f692c0ee36c277925
Mdarifali912 <mdarifali912@gmail.com>
2023-04-03 14:41:15 +0530

    day-1

---
+++ Day-1/autoplay/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Autoplay from './components/Autoplay';
+
+function App() {
+  return (
+    <div className="App">
+      <Autoplay/>
+    </div>
+  );
+}
+
+export default App;
